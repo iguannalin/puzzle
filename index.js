3b15ea243aa04323a387777c4336c5881ed3d977
seasons986 <annalin12345@gmail.com>
2023-08-13 19:29:44 -0400

    cleanup

--- index.js
+++ index.js
@@ -1,39 +1,62 @@
 window.addEventListener("load", () => {
+  // traverse row
+  // traverse col 
+  // find matches for row col
+  // for each match, validate if exists in dictionary with row and col
+
+  let dictionary = {};
+
   function getRandomInt(min, max) {
     min = Math.ceil(min);
     max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
   }
 
-  let rows = [0,1,2,3];
-  const grid = document.getElementById("grid");
-
-  function initGrid(words, rowId) {
-    const tr = document.createElement("tr");
-    tr.id = rowId;
-    if (rowId == 1 || rowId == 2) {
-      for (let i = 0; i< 4; i++) {
-        const td = document.createElement("td");
-        td.innerHTML = `<input type='text' maxlength='1' id=${rowId}-${i}></input>`;
-        tr.appendChild(td);
-      }
-    } else {
-      const randomKey = Object.keys(words)[getRandomInt(0, Object.keys(words).length)];
-      const word = words[randomKey][getRandomInt(0, words[randomKey].length)];
-      word.split("").forEach((letter, index) => {
-        const td = document.createElement("td");
-        td.innerHTML = letter;
-        // if (rowId == 0 && index == 0) td.innerHTML = letter;
-        // else if (rowId == 3 && index == 3) td.innerHTML = letter;
-        // else td.innerHTML = `<input type='text' maxlength='1' id=${rowId}-${index}></input>`;
-        tr.appendChild(td);
-      })
-    }
-    grid.appendChild(tr);
+  function getRandomWord() {
+    const randomKey = Object.keys(dictionary)[getRandomInt(0, Object.keys(dictionary).length)];
+    return dictionary[randomKey][getRandomInt(0, dictionary[randomKey].length)];
   }
 
-  // 4-letter words list scraped from https://eslforums.com/4-letter-words/
-  fetch("https://annaylin.com/100-days/crossword/words.json").then((f) => f.json()).then((r) => {
-    rows.forEach((row) => initGrid(r, row));
+  function getMatches(target) {
+    if (target.length >= 4) return [];
+    return dictionary[target];
+  }
+
+  function solve(grid, r, c) {
+    console.log(grid);
+    if (r < 3 && c > 3) { r += 1; c = 0 };
+    if (r == 3 && c > 3) return grid;
+    let rowStr = grid[r];
+    let colStr = "";
+    grid.forEach((row) => colStr += row[c] ? row[c] : "");
+
+    let result = null;
+
+    if (rowStr && dictionary[rowStr]) {
+      for (let i = 0; i < dictionary[rowStr].length; i++) {
+        let match = dictionary[rowStr][i];
+        if (dictionary[colStr].includes(colStr+match[match.length-1])) {
+          grid[r] = match;
+          let res = solve(grid, r, c+1);
+          if (res) return res;
+        }
+      }
+    } else if (colStr && dictionary[colStr]) {
+      for (let i = 0; i < dictionary[colStr].length; i++) {
+        let match = dictionary[colStr][i];
+        grid[r] = match[r];
+        let res = solve(grid, r, c+1);
+        if (res) return res;
+      }
+    }
+    return result;
+  }
+
+
+  fetch("dictionary.json").then((f) => f.json()).then((r) => {
+    dictionary = r;
+    let starter = "";
+    while (starter.length != 4) starter = getRandomWord();
+    console.log("SOLVED: ", solve([starter,"","",""], 1, 0));
   });
 });
